18b7bc549539fe6ca2228d88705211d63f7eb835
kiriti <kiriti.k999@gmail.com>
2022-04-23 16:12:24 +0530

    update

--- test/javascript/js-practical-questions.js
+++ test/javascript/js-practical-questions.js
@@ -546,6 +546,12 @@ for (let i = 1; i <= number; i++) {
 }
 </Fibanoci>
 
+function fibonacci(n) {
+    return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+console.log(fibonacci(4))
+
 /* #region  Design a parking lot system in javascript */
 // Answer: https://www.jsmount.com/question/write-a-javascript-program-to-design-parking-system/
 /* #endregion */
